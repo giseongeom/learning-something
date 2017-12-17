6d27809fc40819b4a8b9287450370bdc44f5028e
GiSeong Eom <jurist@kldp.org>
2017-12-17 17:28:49 +0900

    [WIP] learning cpp ch1

--- 2017/cpp/jump-into-cpp/ch1/hello.cpp
+++ 2017/cpp/jump-into-cpp/ch1/hello.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+﻿#include <iostream>
 
-// using namespace std;
-int main()
-{
+using namespace std;
+
+int main() {
     std::cout << "Hello world!" << std::endl;
+    return 0;
 }
