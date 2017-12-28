c0e21fe3998198fbbcbab9066f3bdd3bcdad7122
GiSeong Eom <jurist@kldp.org>
2017-12-28 13:27:21 +0900

    Added planned-maintenancecheck

---
+++ 2017/azurerm/planned-maintenance/azure-pmcheck.ps1
@@ -0,0 +1,27 @@
+# See https://docs.microsoft.com/en-us/azure/virtual-machines/windows/maintenance-notifications
+function MyAzureVM-MaintenanceCheck
+{
+   param
+   (
+     [Parameter(Mandatory=$true)]
+     $subscriptionId
+   )
+
+    Select-AzureRmSubscription -SubscriptionId $subscriptionId
+    $rgList= Get-AzureRmResourceGroup 
+
+    for ($rgIdx=0; $rgIdx -lt $rgList.Length ; $rgIdx++)
+    {
+        $rg = $rgList[$rgIdx]
+        $vmList = Get-AzureRMVM -ResourceGroupName $rg.ResourceGroupName
+        for ($vmIdx=0; $vmIdx -lt $vmList.Length ; $vmIdx++)
+        {
+            $vm = $vmList[$vmIdx]
+            $vmDetails = Get-AzureRMVM -ResourceGroupName $rg.ResourceGroupName -Name $vm.Name -Status
+              if ($vmDetails.MaintenanceRedeployStatus)
+              {
+                Write-Output "VM: $($vmDetails.ResourceGroupName) / $($vmDetails.Name)  IsSelfPmAllowed: $($vmDetails.MaintenanceRedeployStatus.IsCustomerInitiatedMaintenanceAllowed)  SelfPmStart: $($vmDetails.MaintenanceRedeployStatus.PreMaintenanceWindowStartTime)  LastOpCode: $($vmDetails.MaintenanceRedeployStatus.LastOperationResultCode)"
+              }
+         }
+    }
+}
