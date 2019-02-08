39952c2e22246fe495cbf436d18a84c2af4649c5
GiSeong Eom <giseong.eom@bluehole.net>
2019-02-08 17:10:48 +0900

    Removed unused/obsoleted stuff

--- obsolete/gollum/_gollum.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-SETLOCAL
-REM See http://stackoverflow.com/questions/16623780/how-to-get-windows-batchs-parent-folder
-for %%i in ("%~dp0..") do set "parent=%%~fi"
-cd /d %USERPROFILE%\Documents\Github\TIL && %parent%\jruby\bin\gollum --config config.rb
-ENDLOCAL
