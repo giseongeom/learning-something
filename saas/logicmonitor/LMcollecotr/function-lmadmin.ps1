a4991b14173f8a51b5f1cb887804bb4ea95506a0
GiSeong Eom <giseong.eom@bluehole.net>
2019-02-20 15:20:36 +0900

    Added LogicMonitor collector-mgmt function

---
+++ saas/logicmonitor/LMcollecotr/function-lmadmin.ps1
@@ -0,0 +1,251 @@
+﻿<# Use TLS 1.2 #>
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# https://www.logicmonitor.com/support/rest-api-developers-guide/v1/collectors/get-collectors/
+Function Get-LMCollectorInfo() {
+    Param(   
+        [Parameter(Mandatory = $true)]
+        [string]$accessId,
+     
+        [Parameter(Mandatory = $true)]
+        [string]$accessKey,
+     
+        [Parameter(Mandatory = $true)]
+        [string]$company,
+
+        [Parameter(Mandatory = $true)]
+        [int]$collectorId
+    )
+
+    BEGIN {
+        <# request details #>
+        $httpVerb = 'GET'
+        $resourcePath = '/setting/collectors/' + $collectorId
+        $queryParams  = ''
+
+        <# Construct URL #>
+        $url = 'https://' + $company + '.logicmonitor.com/santaba/rest' + $resourcePath + $queryParams
+    }
+
+    PROCESS {
+        <# Get current time in milliseconds #>
+        $epoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+
+        <# Concatenate Request Details #>
+        $requestVars = $httpVerb + $epoch + $resourcePath
+
+        <# Construct Signature #>
+        $hmac = New-Object System.Security.Cryptography.HMACSHA256
+        $hmac.Key = [Text.Encoding]::UTF8.GetBytes($accessKey)
+        $signatureBytes = $hmac.ComputeHash([Text.Encoding]::UTF8.GetBytes($requestVars))
+        $signatureHex = [System.BitConverter]::ToString($signatureBytes) -replace '-'
+        $signature = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($signatureHex.ToLower()))
+
+        <# Construct Headers #>
+        $auth = 'LMv1 ' + $accessId + ':' + $signature + ':' + $epoch
+        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        $headers.Add("Authorization",$auth)
+        $headers.Add("Content-Type",'application/json')
+    
+        <# Make Request #>
+        $response = Invoke-RestMethod -Uri $url -Method $httpVerb -Header $headers
+        return $response 
+    }
+
+    END {}
+}
+
+
+# https://www.logicmonitor.com/support/rest-api-developers-guide/v1/collectors/add-a-collector/
+Function Add-LMCollector() {
+    Param(   
+        [Parameter(Mandatory = $true)]
+        [string]$accessId,
+     
+        [Parameter(Mandatory = $true)]
+        [string]$accessKey,
+     
+        [Parameter(Mandatory = $true)]
+        [string]$company
+    )
+
+    BEGIN {
+        <# request details #>
+        $httpVerb = 'POST'
+        $resourcePath = '/setting/collectors/'
+        $data = '{"needAutoCreateCollectorDevice":false}'
+
+        <# Construct URL #>
+        $url = 'https://' + $company + '.logicmonitor.com/santaba/rest' + $resourcePath + $queryParams
+    }
+
+    PROCESS {
+        <# Get current time in milliseconds #>
+        $epoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+
+        <# Concatenate Request Details #>
+        $requestVars = $httpVerb + $epoch + $data + $resourcePath
+
+        <# Construct Signature #>
+        $hmac = New-Object System.Security.Cryptography.HMACSHA256
+        $hmac.Key = [Text.Encoding]::UTF8.GetBytes($accessKey)
+        $signatureBytes = $hmac.ComputeHash([Text.Encoding]::UTF8.GetBytes($requestVars))
+        $signatureHex = [System.BitConverter]::ToString($signatureBytes) -replace '-'
+        $signature = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($signatureHex.ToLower()))
+
+        <# Construct Headers #>
+        $auth = 'LMv1 ' + $accessId + ':' + $signature + ':' + $epoch
+        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        $headers.Add("Authorization",$auth)
+        $headers.Add("Content-Type",'application/json')
+    
+        <# Make Request #>
+        $response = Invoke-RestMethod -Uri $url -Method $httpVerb -Body $data -Header $headers
+        $status = $response.status
+        if ($status -eq 200) {
+            return $response.data.id
+        } else {
+            return -1
+        }
+    }
+
+    END {}
+}
+
+
+# https://www.logicmonitor.com/support/rest-api-developers-guide/v1/collectors/delete-a-collector/
+Function Remove-LMCollector() {
+    Param(   
+        [Parameter(Mandatory = $true)]
+        [string]$accessId,
+     
+        [Parameter(Mandatory = $true)]
+        [string]$accessKey,
+     
+        [Parameter(Mandatory = $true)]
+        [string]$company,
+
+        [Parameter(Mandatory = $true)]
+        [int]$collectorId
+    )
+
+    BEGIN {
+        <# request details #>
+        $httpVerb = 'DELETE'
+        $resourcePath = '/setting/collectors/' + $collectorId
+        $queryParams  = ''
+
+        <# Construct URL #>
+        $url = 'https://' + $company + '.logicmonitor.com/santaba/rest' + $resourcePath + $queryParams
+    }
+
+    PROCESS {
+        <# Get current time in milliseconds #>
+        $epoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+
+        <# Concatenate Request Details #>
+        $requestVars = $httpVerb + $epoch + $resourcePath
+
+        <# Construct Signature #>
+        $hmac = New-Object System.Security.Cryptography.HMACSHA256
+        $hmac.Key = [Text.Encoding]::UTF8.GetBytes($accessKey)
+        $signatureBytes = $hmac.ComputeHash([Text.Encoding]::UTF8.GetBytes($requestVars))
+        $signatureHex = [System.BitConverter]::ToString($signatureBytes) -replace '-'
+        $signature = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($signatureHex.ToLower()))
+
+        <# Construct Headers #>
+        $auth = 'LMv1 ' + $accessId + ':' + $signature + ':' + $epoch
+        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        $headers.Add("Authorization",$auth)
+        $headers.Add("Content-Type",'application/json')
+    
+        <# Make Request #>
+        $response = Invoke-RestMethod -Uri $url -Method $httpVerb -Header $headers
+        $status = $response.status
+        if ($status -eq 200) {
+            return $true
+        } else {
+            return $false
+        }
+    }
+
+    END {}
+}
+
+# https://www.logicmonitor.com/support/rest-api-developers-guide/v1/collectors/downloading-a-collector-installer/
+Function Save-LMCollectorInstaller() {
+    Param(   
+        [Parameter(Mandatory = $true)]
+        [string]$accessId,
+     
+        [Parameter(Mandatory = $true)]
+        [string]$accessKey,
+     
+        [Parameter(Mandatory = $true)]
+        [string]$company,
+
+        [Parameter(Mandatory = $true)]
+        [int]$collectorId,
+        
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Win64","Win32","Linux64","Linux32")]
+        [string]$installerType = 'Win64',
+
+        [Parameter(Mandatory = $false)]
+        [string]$downloadPath = 'c:\windows\temp'
+    )
+
+    BEGIN {
+        <# request details #>
+        $httpVerb = 'GET'
+        $resourcePath = '/setting/collectors/' + $collectorId + '/installers/' + $installerType
+        $queryParams  = ''
+
+        <# Construct URL #>
+        $url = 'https://' + $company + '.logicmonitor.com/santaba/rest' + $resourcePath + $queryParams
+    }
+
+    PROCESS {
+        <# Get current time in milliseconds #>
+        $epoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+
+        <# Concatenate Request Details #>
+        $requestVars = $httpVerb + $epoch + $resourcePath
+
+        <# Construct Signature #>
+        $hmac = New-Object System.Security.Cryptography.HMACSHA256
+        $hmac.Key = [Text.Encoding]::UTF8.GetBytes($accessKey)
+        $signatureBytes = $hmac.ComputeHash([Text.Encoding]::UTF8.GetBytes($requestVars))
+        $signatureHex = [System.BitConverter]::ToString($signatureBytes) -replace '-'
+        $signature = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($signatureHex.ToLower()))
+
+        <# Construct Headers #>
+        $auth = 'LMv1 ' + $accessId + ':' + $signature + ':' + $epoch
+        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        $headers.Add("Authorization",$auth)
+        $headers.Add("Content-Type",'application/json')
+    
+        if ($installerType -in ("Win64","Win32")) {
+            $installer_ext = 'exe'
+        } else {
+            $installer_ext = 'bin'
+        }
+
+        <# Make Request #>       
+        $installer_file = "LM-col-$collectorId-$(get-random)-installer-$installerType.$installer_ext"
+        $installer_file_path = $downloadPath + '\' + $installer_file
+        $progressPreference = 'silentlyContinue'
+        Invoke-WebRequest -Headers $headers -Uri $url -Method $httpVerb -UseBasicParsing -OutFile $installer_file_path
+        if ($?) { 
+            Write-Host "Download location: $installer_file_path" 
+            if (Test-Path $installer_file_path) {
+                return $installer_file_path
+            }
+        } else {
+            return $null
+        }
+    }
+
+    END {}
+}
+
---
+++ saas/logicmonitor/LMcollecotr/readme.md
@@ -0,0 +1,90 @@
+LogicMonitor Collector Management
+============================
+
+## Basic Usage
+
+* dot-sourcing file `function-lmadmin.ps1` is required.
+* Implemented
+  * Common Parameter(s)
+    * `accessId`, `accessKey`, `company`  is always required for authentication
+  * Get-LMCollectorInfo
+    * Parameter(s)
+      * `collectorId` (Required)
+  * Add-LMCollector
+  * Remove-LMCollector
+    * Parameter(s)
+      - `collectorId` (Required)
+  * Save-LMCollectorInstaller
+    * Parameter(s)
+      - `collectorId` (Required)
+      - `installerType` : `Win64` is default
+      - `downloadPath`:  `c:\windows\temp` is default
+
+​    
+
+​    
+
+
+## Add Collector
+
+```PowerShell
+PS > . .\function-lmadmin.ps1
+PS > $accessId  = 'abcdefg'
+PS > $accessKey = '0123456789'
+PS > $company   = 'mycompany'
+
+PS > Add-LMCollector -accessId $accessId -accessKey $accessKey -company $company
+554
+
+PS > $col = Get-LMCollectorInfo -accessId $accessId -accessKey $accessKey -company $company -collectorId 553
+
+PS > $col.data.id
+554
+
+PS > $col.data.platform
+n/a
+
+```
+
+​    
+
+​    
+
+## Download installer
+
+```PowerShell
+PS > Save-LMCollectorInstaller -accessId $accessId -accessKey $accessKey -company $company -collectorId 554
+Download location: c:\windows\temp\LM-col-554-1362504490-installer-Win64.exe
+c:\windows\temp\LM-col-554-1362504490-installer-Win64.exe
+
+PS > $col = Get-LMCollectorInfo -accessId $accessId -accessKey $accessKey -company $company -collectorId 554
+
+PS > $col.data.platform
+windows
+
+```
+
+>  After download is complete, the `platform`attribute of collector is changed to windows or linux. And you can not change the value.
+
+​    
+
+​    
+
+## Delete Collector
+
+```PowerShell
+PS > Remove-LMCollector -accessId $accessId -accessKey $accessKey -company $company -collectorId 554
+True
+```
+
+> Whenever I delete collecotr via REST API,  the status is code always `200`.  Even the `collecotrId` is non-exist on LM side, the result is same. So you may ignore the return value of this function.
+
+​    
+
+​    
+
+## References
+
+* [LogicMonitor - REST API v1 Examples](https://www.logicmonitor.com/support/rest-api-developers-guide/v1/rest-api-v1-examples/)
+* [LogicMonitor - Managing Collectors with the REST API](https://www.logicmonitor.com/support/rest-api-developers-guide/v1/collectors/)
+
