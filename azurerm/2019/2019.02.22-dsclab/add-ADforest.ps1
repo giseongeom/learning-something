e9371baf586450800c12bce0b4b879e2fc9ebec6
GiSeong Eom <jurist@kldp.org>
2019-02-21 22:02:26 +0900

    Add ADDS-script

---
+++ azurerm/2019/2019.02.22-dsclab/add-ADforest.ps1
@@ -0,0 +1,3 @@
+﻿Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+$SafeModeAdministratorPassword = ConvertTo-SecureString -String 'Pa$$w0rd1234!' -AsPlainText -Force
+Install-ADDSForest -DomainName 'dsclab.local' -SafeModeAdministratorPassword $SafeModeAdministratorPassword -InstallDNS -Force
